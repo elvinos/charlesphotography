22bbf5c6fe65fcc86c2592d6c0f0ab967b2816db
elvinos <aep.charles@gmail.com>
2017-07-03 20:33:58 +0100

    Finished Landing Page
    Added Contact Button

--- charleslanguageservices/web/app/themes/sage/index.php
+++ charleslanguageservices/web/app/themes/sage/index.php
@@ -10,11 +10,18 @@
             Site Under Development
           </span>
     </div>
-      <div class="row text-center">
+      <div class="row text-center" id="hammerrow">
             <div id="hammer">
       </div>
 
       </div>
 
+      <div class="row">
+        <div class="col-md  text-center" id="but">
+        <button type="button" id="but2" data-text="Contact" class="buttonFoot">
+            <a href="mailto:helen.charles5@gmail.com">Contact</a>
+        </button>
+      </div>
+      </div>
     </div>
   </div>
