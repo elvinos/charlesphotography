394c2f6d97f714913892373df6ce8cd3d9b338b8
elvinos <aep.charles@gmail.com>
2017-08-11 13:24:30 +0100

    Update Footer -
    Added Elvinos.uk Link and Donate button

--- site/web/app/themes/CharlesPhotos2compact/templates/footer.php
+++ site/web/app/themes/CharlesPhotos2compact/templates/footer.php
@@ -15,7 +15,7 @@
             <div class="row">
                 <div class="col-sm-6"></div>
 
-                <div class="col-sm-6" id= "rightFoot"><span class="footerFont">Site by <a href="http://www.elvinos.wordpress.com">Elvinos Creations</a></span></div>
+                <div class="col-sm-6" id= "rightFoot"><span class="footerFont">Site by <a href="https://www.elvinos.uk">Elvinos</a></span></div>
             </div>
 
         </div>
@@ -46,42 +46,18 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Please Donate To Our Charity <a href="http://www.parityfordisability.org.uk/">Parity For Disability</a></h4>
+            <h3 class="modal-title">Like the Site?</h3>
           </div>
           <div class="modal-body">
-            <h3>Online</h3>
-
-            <span>
-              <a href="https://mydonate.bt.com/charities/parityfordisability">Click here to make a secure donation via our BT MyDonate page</a><br></span>
-
-            <h3>By text</h3>
-
-            <span>Donate by text using our unique code! Text PRTY21 plus the amount to 70070.<br>
-
-            For example, PRTY21 £10</span>
-            <h3>Or donate by bank transfer</h2>
-
-  <span>No fees will be taken out of a bank transfer donation. Parity for Disability's bank details are:
-  <br>
-  CAF Bank Ltd<br>
-  Account no. 00012044<br>
-  Sort Code 40 52 40</span>
-
-
-
-  <h3>Or by post</h3>
-
-  <span>You can post your donation to:<br>
-
-  94 Whetstone Road, <br>
-  Cove, <br>
-  Farnborough, <br>
-  Hants, GU14 9SX<br>
-
-  Cheques should be made out to "Parity for Disability".<br></span>
-
-
-
+            <div class="container-fluid">
+              <row class="text-center">
+              <h3>Make a Small Donation to Keep the Site Going</h3>
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+              <input type="hidden" name="cmd" value="_s-xclick">
+              <input type="hidden" name="hosted_button_id" value="DAN48ZWQLAQKL">
+              <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+              <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+              </form> </div></row>
 
           </div>
           <div class="modal-footer">
